63e12fb5fc2b4fa5b3324f2a9b5a047329c80eed
Kiyoon Kim <yoonkr33@gmail.com>
2020-12-22 07:30:13 +0900

    instagram tools

---
+++ instagram_tools/insta_upload_photo.py
@@ -0,0 +1,6 @@
+from instabot import Bot
+
+bot = Bot()
+bot.login(username="kiyoon0", password="")
+
+bot.upload_photo('avo.jpg', caption="아보프렌즈\n\n#avocado #avofriends #avoground #character #goods #seouldesignfestival #canon #r6 #35mm #아보카도 #아보프렌즈 #캐릭터 #굿즈 #서디페 #서울디자인페스티벌 #캐논 #색감")
