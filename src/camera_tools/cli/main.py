b48e895c154f4c1317e75c1556fad6fe5efc9803
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-03-15 21:17:12 +0900

    .

--- src/camera_tools/cli/main.py
+++ src/camera_tools/cli/main.py
@@ -1,19 +1,27 @@
-import typer
+from cyclopts import App, Parameter
 
+from .. import __version__
 from .bulk_image_resize import bulk_image_resize
 from .datename import datename
 from .fast_image_resize import fast_image_resize
 from .jpgs_to_gif import jpgs_to_gif
 from .organise_images_like_dir import organise_images_like_dir
 
-app = typer.Typer(no_args_is_help=True)
+app = App(
+    help_format="markdown",
+    default_parameter=Parameter(
+        consume_multiple=True,  # Allow list of arguments without repeating --option a --option b ..
+        negative=False,  # Do not make --no-option as a boolean flag
+    ),
+    version=__version__,
+)
 
 
-app.command(no_args_is_help=True)(datename)
-app.command(no_args_is_help=True)(bulk_image_resize)
-app.command(no_args_is_help=True)(fast_image_resize)
-app.command(no_args_is_help=True)(jpgs_to_gif)
-app.command(no_args_is_help=True)(organise_images_like_dir)
+app.command()(datename)
+app.command()(bulk_image_resize)
+app.command()(fast_image_resize)
+app.command()(jpgs_to_gif)
+app.command()(organise_images_like_dir)
 
 
 def main():
